dbf88b6c49f8fc2ce6a9613d6be3e3210c3e5e80
Molter <gabriel.molter@gmail.com>
2016-05-30 21:36:28 -0300

    raw main application

---
+++ chess-app/src/br/com/gabrielmolter/chesschallenge/Main.java
@@ -0,0 +1,10 @@
+package br.com.gabrielmolter.chesschallenge;
+/**
+ * Created by Gabriel Molter on 30/05/2016 at 21:34 at 21:35 BRT.
+ */
+
+public class Main {
+    public static void main(String args[]){
+        System.out.println("Main.main");
+    }
+}
