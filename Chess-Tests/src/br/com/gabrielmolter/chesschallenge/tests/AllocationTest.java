4ec7c7eb3019ca5f6827fff6cdf94b125c7468f3
Molter <gabriel.molter@gmail.com>
2016-06-02 21:17:16 -0300

    test refactor to eliminate code duplication #3

--- Chess-Tests/src/br/com/gabrielmolter/chesschallenge/tests/AllocationTest.java
+++ Chess-Tests/src/br/com/gabrielmolter/chesschallenge/tests/AllocationTest.java
@@ -1,7 +1,9 @@
 package br.com.gabrielmolter.chesschallenge.tests;
 
 import br.com.gabrielmolter.chesschallenge.Pieces.KingPiece;
-import br.com.gabrielmolter.chesschallenge.board.Board;
+import br.com.gabrielmolter.chesschallenge.board.*;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -12,158 +14,168 @@ public class AllocationTest {
 
     private Board mBoard;
 
+    @Before
+    public void setUp(){
+        mBoard = new Board(3, 3);
+    }
+    
+    @After
+    public void tearDown(){
+        mBoard.displayFreeResults();
+    }
+
+
     @Test
     public void validateAllocationFirstLeft(){
-        Board board = new Board(3, 3);
-        board.allocatePiece(0,0, new KingPiece());
-        board.displayFreeResults();
+       
+        mBoard.allocatePiece(0,0, new KingPiece());
 
-        assertTrue(!board.getPiece(0,0).isEmpty());
-        assertTrue(!board.getPiece(0,1).isEmpty());
-        assertTrue(board.getPiece(0,2).isEmpty());
+        assertTrue(!mBoard.getPiece(0,0).isEmpty());
+        assertTrue(!mBoard.getPiece(0,1).isEmpty());
+        assertTrue(mBoard.getPiece(0,2).isEmpty());
 
-        assertTrue(!board.getPiece(1,0).isEmpty());
-        assertTrue(!board.getPiece(1,1).isEmpty());
-        assertTrue(board.getPiece(1,2).isEmpty());
+        assertTrue(!mBoard.getPiece(1,0).isEmpty());
+        assertTrue(!mBoard.getPiece(1,1).isEmpty());
+        assertTrue(mBoard.getPiece(1,2).isEmpty());
 
-        assertTrue(board.getPiece(2,0).isEmpty());
-        assertTrue(board.getPiece(2,1).isEmpty());
-        assertTrue(board.getPiece(2,2).isEmpty());
+        assertTrue(mBoard.getPiece(2,0).isEmpty());
+        assertTrue(mBoard.getPiece(2,1).isEmpty());
+        assertTrue(mBoard.getPiece(2,2).isEmpty());
     }
 
     @Test
     public void validateAllocationFirstMiddle(){
         Board board = new Board(3, 3);
-        board.allocatePiece(0,1, new KingPiece());
-        board.displayFreeResults();
+        mBoard.allocatePiece(0,1, new KingPiece());
+        mBoard.displayFreeResults();
 
-        assertTrue(!board.getPiece(0,0).isEmpty());
-        assertTrue(!board.getPiece(0,1).isEmpty());
-        assertTrue(!board.getPiece(0,2).isEmpty());
+        assertTrue(!mBoard.getPiece(0,0).isEmpty());
+        assertTrue(!mBoard.getPiece(0,1).isEmpty());
+        assertTrue(!mBoard.getPiece(0,2).isEmpty());
 
-        assertTrue(!board.getPiece(1,0).isEmpty());
-        assertTrue(!board.getPiece(1,1).isEmpty());
-        assertTrue(!board.getPiece(1,2).isEmpty());
+        assertTrue(!mBoard.getPiece(1,0).isEmpty());
+        assertTrue(!mBoard.getPiece(1,1).isEmpty());
+        assertTrue(!mBoard.getPiece(1,2).isEmpty());
 
-        assertTrue(board.getPiece(2,0).isEmpty());
-        assertTrue(board.getPiece(2,1).isEmpty());
-        assertTrue(board.getPiece(2,2).isEmpty());
+        assertTrue(mBoard.getPiece(2,0).isEmpty());
+        assertTrue(mBoard.getPiece(2,1).isEmpty());
+        assertTrue(mBoard.getPiece(2,2).isEmpty());
     }
 
     @Test
     public void validateAllocationFirstRight(){
         Board board = new Board(3, 3);
-        board.allocatePiece(0,2, new KingPiece());
-        board.displayFreeResults();
+        mBoard.allocatePiece(0,2, new KingPiece());
+        mBoard.displayFreeResults();
 
-        assertTrue(board.getPiece(0,0).isEmpty());
-        assertTrue(!board.getPiece(0,1).isEmpty());
-        assertTrue(!board.getPiece(0,2).isEmpty());
+        assertTrue(mBoard.getPiece(0,0).isEmpty());
+        assertTrue(!mBoard.getPiece(0,1).isEmpty());
+        assertTrue(!mBoard.getPiece(0,2).isEmpty());
 
-        assertTrue(board.getPiece(1,0).isEmpty());
-        assertTrue(!board.getPiece(1,1).isEmpty());
-        assertTrue(!board.getPiece(1,2).isEmpty());
+        assertTrue(mBoard.getPiece(1,0).isEmpty());
+        assertTrue(!mBoard.getPiece(1,1).isEmpty());
+        assertTrue(!mBoard.getPiece(1,2).isEmpty());
 
-        assertTrue(!board.getPiece(2,0).isEmpty());
-        assertTrue(!board.getPiece(2,1).isEmpty());
-        assertTrue(!board.getPiece(2,2).isEmpty());
+        assertTrue(!mBoard.getPiece(2,0).isEmpty());
+        assertTrue(!mBoard.getPiece(2,1).isEmpty());
+        assertTrue(!mBoard.getPiece(2,2).isEmpty());
     }
 
     @Test
     public void validateAllocationMidleLeft(){
         Board board = new Board(3, 3);
-        board.allocatePiece(1,0, new KingPiece());
-        board.displayFreeResults();
+        mBoard.allocatePiece(1,0, new KingPiece());
+        mBoard.displayFreeResults();
 
-        assertTrue(!board.getPiece(0,0).isEmpty());
-        assertTrue(!board.getPiece(0,1).isEmpty());
-        assertTrue(board.getPiece(0,2).isEmpty());
+        assertTrue(!mBoard.getPiece(0,0).isEmpty());
+        assertTrue(!mBoard.getPiece(0,1).isEmpty());
+        assertTrue(mBoard.getPiece(0,2).isEmpty());
 
-        assertTrue(!board.getPiece(1,0).isEmpty());
-        assertTrue(!board.getPiece(1,1).isEmpty());
-        assertTrue(board.getPiece(1,2).isEmpty());
+        assertTrue(!mBoard.getPiece(1,0).isEmpty());
+        assertTrue(!mBoard.getPiece(1,1).isEmpty());
+        assertTrue(mBoard.getPiece(1,2).isEmpty());
 
-        assertTrue(!board.getPiece(2,0).isEmpty());
-        assertTrue(!board.getPiece(2,1).isEmpty());
-        assertTrue(board.getPiece(2,2).isEmpty());
+        assertTrue(!mBoard.getPiece(2,0).isEmpty());
+        assertTrue(!mBoard.getPiece(2,1).isEmpty());
+        assertTrue(mBoard.getPiece(2,2).isEmpty());
     }
 
 
     @Test
     public void validateAllocationMiddleMiddle(){
         Board board = new Board(3, 3);
-        board.allocatePiece(1,1, new KingPiece());
-        board.displayFreeResults();
+        mBoard.allocatePiece(1,1, new KingPiece());
+        mBoard.displayFreeResults();
 
-        assertFalse(board.getPiece(0,0).isEmpty());
-        assertFalse(board.getPiece(0,1).isEmpty());
-        assertFalse(board.getPiece(0,2).isEmpty());
+        assertFalse(mBoard.getPiece(0,0).isEmpty());
+        assertFalse(mBoard.getPiece(0,1).isEmpty());
+        assertFalse(mBoard.getPiece(0,2).isEmpty());
 
-        assertFalse(board.getPiece(1,0).isEmpty());
-        assertFalse(board.getPiece(1,1).isEmpty());
-        assertFalse(board.getPiece(1,2).isEmpty());
+        assertFalse(mBoard.getPiece(1,0).isEmpty());
+        assertFalse(mBoard.getPiece(1,1).isEmpty());
+        assertFalse(mBoard.getPiece(1,2).isEmpty());
 
-        assertFalse(board.getPiece(2,0).isEmpty());
-        assertFalse(board.getPiece(2,1).isEmpty());
-        assertFalse(board.getPiece(2,2).isEmpty());
+        assertFalse(mBoard.getPiece(2,0).isEmpty());
+        assertFalse(mBoard.getPiece(2,1).isEmpty());
+        assertFalse(mBoard.getPiece(2,2).isEmpty());
     }
 
     @Test
     public void validateAllocationMidleRight(){
         Board board = new Board(3, 3);
-        board.allocatePiece(1,2, new KingPiece());
-        board.displayFreeResults();
+        mBoard.allocatePiece(1,2, new KingPiece());
+        mBoard.displayFreeResults();
 
-        assertTrue(board.getPiece(0,0).isEmpty());
-        assertTrue(!board.getPiece(0,1).isEmpty());
-        assertTrue(!board.getPiece(0,2).isEmpty());
+        assertTrue(mBoard.getPiece(0,0).isEmpty());
+        assertTrue(!mBoard.getPiece(0,1).isEmpty());
+        assertTrue(!mBoard.getPiece(0,2).isEmpty());
 
-        assertTrue(board.getPiece(1,0).isEmpty());
-        assertTrue(!board.getPiece(1,1).isEmpty());
-        assertTrue(!board.getPiece(1,2).isEmpty());
+        assertTrue(mBoard.getPiece(1,0).isEmpty());
+        assertTrue(!mBoard.getPiece(1,1).isEmpty());
+        assertTrue(!mBoard.getPiece(1,2).isEmpty());
 
-        assertTrue(board.getPiece(2,0).isEmpty());
-        assertTrue(!board.getPiece(2,1).isEmpty());
-        assertTrue(!board.getPiece(2,2).isEmpty());
+        assertTrue(mBoard.getPiece(2,0).isEmpty());
+        assertTrue(!mBoard.getPiece(2,1).isEmpty());
+        assertTrue(!mBoard.getPiece(2,2).isEmpty());
     }
 
     @Test
     public void validateAllocationBottonRight(){
         Board board = new Board(3, 3);
-        board.allocatePiece(2,2, new KingPiece());
-        board.displayFreeResults();
+        mBoard.allocatePiece(2,2, new KingPiece());
+        mBoard.displayFreeResults();
 
-        assertTrue(board.getPiece(0,0).isEmpty());
-        assertTrue(board.getPiece(0,1).isEmpty());
-        assertTrue(board.getPiece(0,2).isEmpty());
+        assertTrue(mBoard.getPiece(0,0).isEmpty());
+        assertTrue(mBoard.getPiece(0,1).isEmpty());
+        assertTrue(mBoard.getPiece(0,2).isEmpty());
 
-        assertTrue(board.getPiece(1,0).isEmpty());
-        assertTrue(!board.getPiece(1,1).isEmpty());
-        assertTrue(!board.getPiece(1,2).isEmpty());
+        assertTrue(mBoard.getPiece(1,0).isEmpty());
+        assertTrue(!mBoard.getPiece(1,1).isEmpty());
+        assertTrue(!mBoard.getPiece(1,2).isEmpty());
 
-        assertTrue(!board.getPiece(2,0).isEmpty());
-        assertTrue(!board.getPiece(2,1).isEmpty());
-        assertTrue(!board.getPiece(2,2).isEmpty());
+        assertTrue(!mBoard.getPiece(2,0).isEmpty());
+        assertTrue(!mBoard.getPiece(2,1).isEmpty());
+        assertTrue(!mBoard.getPiece(2,2).isEmpty());
     }
 
 
     @Test
     public void validateAllocationBottonLeft(){
         Board board = new Board(3, 3);
-        board.allocatePiece(2,0, new KingPiece());
-        board.displayFreeResults();
+        mBoard.allocatePiece(2,0, new KingPiece());
+        mBoard.displayFreeResults();
 
-        assertTrue(board.getPiece(0,0).isEmpty());
-        assertTrue(board.getPiece(0,1).isEmpty());
-        assertTrue(board.getPiece(0,2).isEmpty());
+        assertTrue(mBoard.getPiece(0,0).isEmpty());
+        assertTrue(mBoard.getPiece(0,1).isEmpty());
+        assertTrue(mBoard.getPiece(0,2).isEmpty());
 
-        assertTrue(!board.getPiece(1,0).isEmpty());
-        assertTrue(!board.getPiece(1,1).isEmpty());
-        assertTrue(board.getPiece(1,2).isEmpty());
+        assertTrue(!mBoard.getPiece(1,0).isEmpty());
+        assertTrue(!mBoard.getPiece(1,1).isEmpty());
+        assertTrue(mBoard.getPiece(1,2).isEmpty());
 
-        assertTrue(!board.getPiece(2,0).isEmpty());
-        assertTrue(!board.getPiece(2,1).isEmpty());
-        assertTrue(board.getPiece(2,2).isEmpty());
+        assertTrue(!mBoard.getPiece(2,0).isEmpty());
+        assertTrue(!mBoard.getPiece(2,1).isEmpty());
+        assertTrue(mBoard.getPiece(2,2).isEmpty());
     }
 
     @Test
